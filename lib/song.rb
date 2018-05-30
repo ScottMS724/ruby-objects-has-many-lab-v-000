a6e7c327266cd0a3a47e19b3dc785b7a7d083a4d
Scott Silverman <scottms724@gmail.com>
2018-05-30 15:59:11 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  attr_accessor :name, :artist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << name 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def artist_name
+    if self.artist.name
+      self.artist.name 
+    else 
+      nil 
+    end
+  end 
+  
+end 
